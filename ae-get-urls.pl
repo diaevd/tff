6b3f8ad21cfa29e4655d00c88c2a5db86208e962
Evgeny Duzhakov <diaevd@gmail.com>
2016-06-26 18:35:31 +0300

    ae-get-urls.pl: Add loop with urls download urls-for-ae-get-urls.txt: add error url

--- ae-get-urls.pl
+++ ae-get-urls.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 use strict;
+use AnyEvent;
+use AnyEvent::HTTP;
+use Time::HiRes qw/time/;
 use Regexp::Common qw/URI/;
 use File::Basename;
 
@@ -20,13 +23,61 @@ my @urls = ();
 while (my $url = <>) {
 	chomp($url);
 	if($url !~ /($RE{URI}{HTTP})/) {
-		warn "INVALID URL: \"$url\n";
+		warn "URL: \"$url\" is not valid\n";
 	} else {
 		push @urls, $url;
 	}
 }
 
-warn "URL: $_\n" for (@urls);
+#warn "URL: $_\n" for (@urls);
 
-1;
+my $start_time = time;
+my $result;
+my $count = 0;
+
+my $cv = AnyEvent->condvar();
+
+$cv->begin(sub { shift->send($result) });
+
+for my $url (@urls) {
+
+	$cv->begin;
+	my $now = time;
+
+	my $request;
+	$request = http_request(
+		GET => $url,
+		timeout => 2, # seconds
+		sub {
+			my ($body, $hdr) = @_;
+			$count++;
+			if ($hdr->{Status} =~ /^2/) {
+				push (@$result, join("\t", (qq/[$count] "$url"/,
+							    " length: ",
+							    $hdr->{'content-length'},
+							    " time: ",
+							    sprintf("%.3f", time - $now),
+							    "ms"))
+			      );
+				print $body;
+			} else {
+				push (@$result, join("", qq/[$count] "$url"/,
+						     ": (",
+						     $hdr->{Status},
+						     ") ",
+						     $hdr->{Reason})
+			      );
+			}
+			undef $request;
+			$cv->end;
+		}
+	);
+}
+
+$cv->end;
+my $res = $cv->recv;
+
+print "\n" . "-" x 10 . "\n" . join("\n", @$res) . "\n" . "-" x 10 . "\n" if(defined($res));
+print "Total elapsed time: " . sprintf("%.3f", time - $start_time) . "ms\n";
+
 __END__
