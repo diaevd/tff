bec5664c13e1634142790783bbab3845251518b0
Evgeny Duzhakov <diaevd@gmail.com>
2016-12-02 19:20:47 +0300

    Fix knight move test

--- go/chess_knight_moves/knight_moves_test.go
+++ go/chess_knight_moves/knight_moves_test.go
@@ -9,13 +9,15 @@ func TestSquareString(t *testing.T) {
 
 	s1 := Square{1, 1}
 	str1 := s1.String()
-	if str1 != "a1" {
-		t.Errorf("SquareString: expected a1, got %s", str1)
+	res1 := "a1"
+	if str1 != res1 {
+		t.Errorf("SquareString: expected %s, got %s", res1, str1)
 	}
 	s2 := Square{8, 8}
 	str2 := s2.String()
-	if str2 != "h8" {
-		t.Errorf("SquareString: expected a1, got %s", str2)
+	res2 := "h8"
+	if str2 != res2 {
+		t.Errorf("SquareString: expected %s, got %s", res2, str2)
 	}
 }
 
@@ -25,28 +27,32 @@ func TestKnightLegalMoves(t *testing.T) {
 	pos := NewSquare(2, 1)
 	moves := pos.KnightLegalMoves()
 	str := strings.Join(moves, " ")
-	if str != "Nc3 Na3 Nd2" {
-		t.Errorf("KnightLegalMoves: expected [Nc3 Na3 Nd2], got [%s]", str)
+	res := "Nc3 Na3 Nd2"
+	if str != res {
+		t.Errorf("KnightLegalMoves: expected [%s], got [%s]", res, str)
 	}
 	// c3
 	pos = NewSquare(3, 3)
 	moves = pos.KnightLegalMoves()
 	str = strings.Join(moves, " ")
-	if str != "Nd5 Nb5 Nd1 Nb1 Ne4 Na4 Ne2 Na2" {
-		t.Errorf("KnightLegalMoves: expected [Nd5 Nb5 Nd1 Nb1 Ne4 Na4 Ne2 Na2], got [%s]", str)
+	res = "Nd5 Nb5 Nd1 Nb1 Ne4 Na4 Ne2 Na2"
+	if str != res {
+		t.Errorf("KnightLegalMoves: expected [%s], got [%s]", res, str)
 	}
 	// a3
 	pos = NewSquare(1, 3)
 	moves = pos.KnightLegalMoves()
 	str = strings.Join(moves, " ")
-	if str != "Nb5 Nb1 Nc4 Nc2" {
-		t.Errorf("KnightLegalMoves: expected [Nb5 Nb1 Nc4 Nc2], got [%s]", str)
+	res = "Nb5 Nb1 Nc4 Nc2"
+	if str != res {
+		t.Errorf("KnightLegalMoves: expected [%s], got [%s]", res, str)
 	}
 	// d2
 	pos = NewSquare(4, 2)
 	moves = pos.KnightLegalMoves()
 	str = strings.Join(moves, " ")
-	if str != "Ne4 Nc4 Nf3 Nb3 Nf1 Nb1" {
-		t.Errorf("KnightLegalMoves: expected [Ne4 Nc4 Nf3 Nb3 Nf1 Nb1], got [%s]", str)
+	res = "Ne4 Nc4 Nf3 Nb3 Nf1 Nb1"
+	if str != res {
+		t.Errorf("KnightLegalMoves: expected [%s], got [%s]", res, str)
 	}
 }
