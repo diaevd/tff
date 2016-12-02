07d9531da816f993923e9dbd1721c4cff2144a55
Evgeny Duzhakov <diaevd@gmail.com>
2016-12-02 18:12:18 +0300

    Add knight moves

--- go/chess_knight_moves/knight_moves_test.go
+++ go/chess_knight_moves/knight_moves_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestSquareString(t *testing.T) {
 
@@ -13,5 +16,37 @@ func TestSquareString(t *testing.T) {
 	str2 := s2.String()
 	if str2 != "h8" {
 		t.Errorf("SquareString: expected a1, got %s", str2)
+	}
+}
+
+func TestKnightLegalMoves(t *testing.T) {
+
+	// b1
+	pos := NewSquare(2, 1)
+	moves := pos.KnightLegalMoves()
+	str := strings.Join(moves, " ")
+	if str != "Nc3 Na3 Nd2" {
+		t.Errorf("KnightLegalMoves: expected [Nc3 Na3 Nd2], got [%s]", str)
+	}
+	// c3
+	pos = NewSquare(3, 3)
+	moves = pos.KnightLegalMoves()
+	str = strings.Join(moves, " ")
+	if str != "Nd5 Nb5 Nd1 Nb1 Ne4 Na4 Ne2 Na2" {
+		t.Errorf("KnightLegalMoves: expected [Nd5 Nb5 Nd1 Nb1 Ne4 Na4 Ne2 Na2], got [%s]", str)
+	}
+	// a3
+	pos = NewSquare(1, 3)
+	moves = pos.KnightLegalMoves()
+	str = strings.Join(moves, " ")
+	if str != "Nb5 Nb1 Nc4 Nc2" {
+		t.Errorf("KnightLegalMoves: expected [Nb5 Nb1 Nc4 Nc2], got [%s]", str)
+	}
+	// d2
+	pos = NewSquare(4, 2)
+	moves = pos.KnightLegalMoves()
+	str = strings.Join(moves, " ")
+	if str != "Ne4 Nc4 Nf3 Nb3 Nf1 Nb1" {
+		t.Errorf("KnightLegalMoves: expected [Ne4 Nc4 Nf3 Nb3 Nf1 Nb1], got [%s]", str)
 	}
 }
